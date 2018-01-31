720929781422a53cb0d9e79d5d2b946d0b6fa53a
DESKTOP-MDAVLOT\songge <songge040701@aliyun.com>
2018-01-31 11:15:08 +0800

    自定义ClassLoader

---
+++ a_java_foundation/src/com/songge/java_foundation/SonggeClassLoader.java
@@ -0,0 +1,12 @@
+package com.songge.java_foundation;
+
+/**  
+* @ClassName: SonggeClassLoader  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author songge  
+* @date 2018年1月31日  
+*    
+*/
+public class SonggeClassLoader extends ClassLoader {
+
+}
